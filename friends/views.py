35fec9aa398d6fc27e4cfca6cb964310ba68deeb
Takematsu <kaientai100@gmail.com>
2022-04-23 12:20:47 +0900

    add create.view

--- friends/views.py
+++ friends/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import Friend
-from django.shortcuts import render
-from .forms import HelloForm
+from .forms import HelloForm, FriendForm
 from django.http import HttpResponse
 
 # Create your views here.
@@ -9,37 +8,30 @@ def index(request):
     data = Friend.objects.all()
     context ={
         'title':'Hello Friend',
-        'massage':'all friends',
         'data': data,
     }
     return render(request, 'friends/index.html', context)
-
-    '''
-     if (request.method == 'POST'):
-        num = request.POST['id']
-        item = Friend.objects.get(id=num)
-        context['data'] = [item]
+    
+    ''' if (request.method == 'POST'):#もし、リクエストがPOSTであれば、以下の処理をせよ！
+        num = request.POST['id']#入力されたidナンバーをnumに代入せよ！
+        item = Friend.objects.get(id=num)#numに代入されたrequest.POST['id']を　idとして、データを取得せよ！
+        context['data'] = [item] #取得したデータ（item）で、'data'を上書きせよ！
         context['form'] = HelloForm(request.POST)
     else:
         context['data'] = Friend.objects.all() 
+    
+    return render(request, 'friends/index.html', context) '''
+    
+def create(request):
 
-    return render(request, 'friends/index.html', context)
-
-    def create(request):
-        context = {
-           'title':'Hello Friend',
-            'form':HelloForm()
-        }
-
-        if(request.method =='POST'):
-            name = request.POST['name']
-            mail = request.POST['mail']
-            male = 'male' in request.POST
-            age = request.POST['age']
-            birth = request.POST['birthday']
-
-            friend = Friend(name=name,mail=mail,male=male,age=age,birthday=birth)
-            friend.save()
-            return redirect(to='/friends')
-        return render(request, 'friends/index.html', context)
-'''
+    if(request.method =='POST'):
+        obj = Friend()
+        friend = FriendForm(request.POST, instance=obj)
+        friend.save()
+        return redirect(to='/friends')
+    
+    context = {
+        'title':'Hello Friend',
+        'form':FriendForm(),
+    }
+    return render(request, 'friends/create.html', context)
